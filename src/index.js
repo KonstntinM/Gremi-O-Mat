a66ea2db11cfe23e93069b330d6c8d15a4e55e48
Konstantin <k_marx@gmx.de>
2020-08-28 17:26:50 +0200

    create index.js

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+var app = express();
+
+const config = require('yaml').parse(require('fs').readFileSync(__dirname + '/config/config.yaml', 'utf8'))
+const allowCrossOrigin = require("./services/allowCrossOrigin");
+
+app.use(express.json())
+app.use(bodyParser.raw({type: 'application/json'}));
+app.use(cookieParser())
+
+app.use(allowCrossOrigin);
+
+app.get("/", function(req, res) {
+  res.json("Hello World from Express!");
+});
+
+const port = config.Server[0].PORT || 80
+app.listen(port, () => {
+  console.log("\x1b[32m" + "The backend server started successfully and listens to port " + "\x1b[33m" + port + "\x1b[32m" + "." + "\x1b[0m")
+});
